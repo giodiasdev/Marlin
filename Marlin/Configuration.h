fdaa7625ab68b27760fbf0ce449a9f52967d5ff5
Matheus Dias <34991230+giodiasdev@users.noreply.github.com>
2023-05-27 15:18:25 -0300

    Added machine UUID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -62,7 +62,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Dust, Ender-3 Pro)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(giodiasdev, Ender-3 Pro)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -141,7 +141,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "632c5765-2b17-4c06-91ea-d46e1c9ce0e9"
 
 // @section stepper drivers
 
